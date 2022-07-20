e5567f010fa35ca774adb0fc457b990acb3dd3f5
Pascal alban <alban.pascal@laposte.net>
2022-07-20 15:20:09 +0200

    mise en place de l'environement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Layout from "components/UI/Layout";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "components/UI/Layout";
+
+import Home from "components/Home";
+import About from "components/About";
 
 export default function App() {
   return (
-    <Layout>
-      <h1>Hello world</h1>
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <h1>Home page</h1>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div``;
